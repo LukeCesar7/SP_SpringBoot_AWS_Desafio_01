dedb7663c9a5755e7f2acc9f631442e9db6be3ef
Luquian Cesar <luquian.santos@outlook.com>
2024-10-07 16:25:42 -0300

    fix: MenuService implemented and JPA implements. Database 100% functional

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,9 @@
 package application;
+import entities.services.MenuService;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,8 +12,17 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_test");
+        EntityManager em = emf.createEntityManager();
+
+        MenuService menuService = new MenuService(sc, dTF, em);
+
+        menuService.menu();
 
         sc.close();
-
+        em.close();
+        emf.close();
     }
 }
